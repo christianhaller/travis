8f67bc98ce19fc7d360542ffa6c72dc4089e075d
Christian Haller <hallo@christianhaller.com>
2015-11-15 23:51:17 +0100

    url

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
     var url = 'http://www.tripadvisor.com/members/christianhaller';
     casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:42.0) Gecko/20100101 Firefox/42.0');
 
+
+    casper.on("resource.error", function(resourceError){
+        console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+        console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+    });
+
     casper.test.begin('basic functions', function (test) {
 
         casper.start('http://stage.download-your-travelmap.christianhaller.com/?url='+url, function () {
