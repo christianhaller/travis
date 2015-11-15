6433fc30392f033f7b2f7228ad2af02b0af77d01
Christian Haller <hallo@christianhaller.com>
2015-11-15 16:29:41 +0100

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+/*global casper */
+(function (casper) {
+    'use strict';
+    var url = 'http://www.tripadvisor.com/members/christianhaller';
+    casper.test.begin('basic functions', function (test) {
+
+        casper.start('http://stage.download-your-travelmap.christianhaller.com/', function () {
+            this.echo(this.getCurrentUrl());
+            casper.fill('.url-form', {
+                'url': url
+            }, true);
+
+            this.wait(7000, function() {
+                this.echo(this.getCurrentUrl());
+                test.assertEvalEquals(function () {
+                    return $('.js-username').text();
+                }, 'christianhaller', 'correct name');
+            });
+        });
+
+
+        casper.run(function () {
+            test.done();
+        });
+    });
+
+}(casper));
+
