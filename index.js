d7652b3005eb01e3d18803e573f7abb8b9a8115d
Haller, Christian <haller.christian@guj.de>
2015-11-16 13:26:11 +0100

    should work

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 /*global casper */
 (function (casper) {
-    'use strict';
-    var url = 'http://www.tripadvisor.com/members/christianhaller';
-    //casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:42.0) Gecko/20100101 Firefox/42.0');
+	'use strict';
+	var tripAdvisorProfileUrl = 'http://www.tripadvisor.com/members/christianhaller',
+		stageUrl = 'http://stage.download-your-travelmap.christianhaller.com/';
+	//casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:42.0) Gecko/20100101 Firefox/42.0');
+
+	casper.options.viewportSize = {width: 1600, height: 950};
+	casper.on("resource.error", function (resourceError) {
+		console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+		console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+	});
 
 
-    casper.on("resource.error", function(resourceError){
-        console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
-        console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
-    });
-
-    casper.test.begin('basic functions', function (test) {
-
-        casper.start('http://stage.download-your-travelmap.christianhaller.com/?url='+url, function () {
+	casper.test.begin('basic functions', function (test) {
 
 
+		casper.start(stageUrl, function () {
+			this.evaluate(function () {
+				return __utils__.findOne('.url-form').setAttribute('action', stageUrl);
+			});
+			this.fill('.url-form', { url: tripAdvisorProfileUrl }, true);
+
+			this.waitForUrl(stageUrl + '?url=' + tripAdvisorProfileUrl, function () {
+				console.log(this.getCurrentUrl());
+				test.assertEquals(this.fetchText('.js-username'), 'christianhaller', 'name');
+			});
 
 
-
-        });
-        casper.then(function(){
-            this.wait(8000, function() {
-                test.assertEvalEquals(function () {
-                    return __utils__.findOne('.js-username').textContent;
-                    //return $('.js-username').text();
-                }, 'christianhaller', 'correct name');
-            });
+		});
 
 
-
-        });
-
-
-        casper.run(function () {
-            test.done();
-        });
-    });
+		casper.run(function () {
+			test.done();
+		});
+	});
 
 }(casper));
 
