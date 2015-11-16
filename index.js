8c0d03c0709e80f36d98f57c94c9b9c27be764c2
Christian Haller <hallo@christianhaller.com>
2015-11-17 00:03:11 +0100

    slimerjs

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 	'use strict';
 	var tripAdvisorProfileUrl = 'http://www.tripadvisor.com/members/christianhaller',
 		stageUrl = 'http://stage.download-your-travelmap.christianhaller.com/';
-	//casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:42.0) Gecko/20100101 Firefox/42.0');
-
+	
 	casper.options.viewportSize = {width: 1600, height: 950};
 	casper.on("resource.error", function (resourceError) {
 		console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
@@ -16,15 +15,13 @@
 
 
 		casper.start(stageUrl, function () {
-			this.evaluate(function () {
-				return __utils__.findOne('.url-form').setAttribute('action', stageUrl);
-			});
-			this.fill('.url-form', { url: tripAdvisorProfileUrl }, true);
 
+			this.fill('.url-form', { url: tripAdvisorProfileUrl }, true);
+            console.log(this.getCurrentUrl());
 			this.waitForUrl(stageUrl + '?url=' + tripAdvisorProfileUrl, function () {
 				console.log(this.getCurrentUrl());
 				test.assertEquals(this.fetchText('.js-username'), 'christianhaller', 'name');
-			});
+			},function(){},9009);
 
 
 		});
